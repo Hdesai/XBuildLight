cfd8e53e2d88cda30625679d3b9ea576c015678d
Himanshu Desai <himanshu.desai@icarehealth.com>
2015-10-12 21:46:01 +1100

    Using VS Setup and Deployment Project

---
+++ BuildClient/BinaryConsoleTraceListener.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildClient
+{
+    public class BinaryConsoleTraceListener : ConsoleTraceListener
+    {
+        public override void WriteLine(string message)
+        {
+            byte[] arr = System.Text.Encoding.ASCII.GetBytes(message);
+            var sb = new System.Text.StringBuilder();
+            foreach (byte b in arr)
+            {
+                sb.Append(Convert.ToString((int) b, 2));
+            }
+            string binary = sb.ToString();
+            base.WriteLine(binary);
+        }
+    }
+}
